0c99be70cf23dfa665e881160940efc47733f293
IvanGitHub7 <usoltsev-ivan94@mail.ru>
2025-01-05 21:25:50 +0700

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,23 @@
+from os import stat_result
+from pydantic import BaseModel
+
+class CreateUser(BaseModel):
+    username: str
+    firstname: str
+    lastname: str
+    age: int
+
+class UpdateUser(BaseModel):
+    firstname: str
+    lastname: str
+    age: int
+
+class CreateTask(BaseModel):
+    title: str
+    content: str
+    priority: int
+
+class UpdateTask(BaseModel):
+    title: str
+    content: str
+    priority: int
